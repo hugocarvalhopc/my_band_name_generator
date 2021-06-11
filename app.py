5871aefde702404c609525ee0dbc8828e53455ef
hugocarvalhopc <hugosimoes172002@gmail.com>
2021-06-11 17:59:51 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+print('-=-' * 20)
+print('{:^60}'.format('BEM VINDO AO GERADOR DE NOMES DE BANDA'))
+print('-=-' * 20)
+
+nome_cidade = input('\nQual o nome da cidade onde você mora? ')
+
+nome_pet = input("\nQual o nome do seu pet? ")
+
+print("\nO nome escolhido para sua banda foi: " + nome_cidade + ' ' + nome_pet)
+
